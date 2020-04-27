8dad28696166e61da04e9ce3c6ca5c32dbfe9ac4
Uri Klar <uriklar@gmail.com>
2020-04-27 08:30:07 +0300

    added purge css

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,19 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  // Specify the paths to all of the template files in your project
+  content: ["./pages/**/*.jsx", "./components/**/*.jsx"],
+
+  // make sure css reset isnt removed on html and body
+  whitelist: ["html", "body"],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
+
 module.exports = {
-  plugins: ["postcss-import", "tailwindcss", "autoprefixer"],
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    purgecss,
+    //...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+  ],
 };
