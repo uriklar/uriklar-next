9031dfa7b0dcb727946ab1bfacc3785a4fe63847
Uri Klar <uriklar@gmail.com>
2025-07-18 12:35:33 -0400

    copy updates

--- components/newsletter.jsx
+++ components/newsletter.jsx
@@ -23,24 +23,40 @@ export default () => {
       <h2 className="text-3xl font-bold text-center">
         I also have a newsletter!
       </h2>
-      <p className="mt-2 font-light w-4/5 text-center leading-relaxed">
+      <p
+        className="mt-2 font-light w-4/5 text-center leading-relaxed"
+        style={{ textDecoration: "line-through" }}
+      >
         It includes intersting stuff about tech and will arrive to your mailbox
         no more than once every 2 weeks
       </p>
+      <p className="mt-2 font-light w-4/5 text-center leading-relaxed">
+        I don't maintain this newsletter anymore, but if you want a tutorial on
+        how I built it and integrated it in my site you can watch it here:
+        <a
+          href="https://www.youtube.com/watch?v=Rzlop3Bgk1Q"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-600 hover:text-blue-800 underline ml-1"
+        >
+          &nbsp;YouTube Tutorial
+        </a>
+      </p>
       <div className="flex w-1/2 lg:w-2/3 justify-center mt-5 flex-col lg:flex-row">
         <input
-          className="appearance-none mb-2 lg:mb-0 w-full lg:w-2/3 border border-gray-500 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:border-gray-600"
+          className="appearance-none mb-2 lg:mb-0 w-full lg:w-2/3 border border-gray-500 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:border-gray-600 opacity-50 cursor-not-allowed"
           type="text"
           placeholder="Enter Email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          disabled={true}
         />
         <button
-          className={`lg:ml-2 w-full lg:w-1/3 shadow bg-brand2 focus:shadow-outline focus:outline-none text-center text-white font-bold py-2 px-4 rounded flex ${
+          className={`lg:ml-2 shadow bg-brand2 focus:shadow-outline focus:outline-none text-center text-white font-bold py-2 px-4 rounded flex opacity-50 cursor-not-allowed ${
             state === "LOADING" ? "button-gradient-loading" : ""
           }`}
           type="button"
-          disabled={state === "LOADING"}
+          disabled={true}
           onClick={subscribe}
         >
           Subscribe
